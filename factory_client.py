37966c2c24888b0a0cb4899fd3f49188464a290c
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2020-04-16 14:39:58 +0800

    Update factory_client.py

--- factory_client.py
+++ factory_client.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import win32com.client as win32
 import sys
+import os
 
 para = sys.argv[1]
 excel = win32.gencache.EnsureDispatch("Excel.Application")
-wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\" + str(para)+".xlsx")
+path = os.getcwd()
+wb = excel.Workbooks.Open(str(path)+"\\"+ str(para)+".xlsx")
 #excel.Visible = True
 ws = wb.Worksheets("工作表1")
 
@@ -14,13 +16,13 @@ s.readline()
 text = list()
 for line in f:
     str_list = line.split(',')
-  
     if str_list[0] == "WRITE":
         ws.Cells(int(str_list[1]),int(str_list[2])).Value = str_list[3]
         s.write("OK\n")
         wb.Save()
         
     elif str_list[0] == "Get_Data":
+        s.write("OK\n")
         get_data_list = ws.Range(ws.Cells(1,1), ws.Cells(200,100)).Value
         for i in range (200):
             for j in range(100):
