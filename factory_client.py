380e8ef7921fcd574f93503bb57bea23864e61cd
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2020-04-16 14:20:34 +0800

    Update factory_client.py

--- factory_client.py
+++ factory_client.py
@@ -4,7 +4,7 @@ import sys
 
 para = sys.argv[1]
 excel = win32.gencache.EnsureDispatch("Excel.Application")
-wb = excel.Workbooks.Open(str(para))
+wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\" + str(para)+".xlsx")
 #excel.Visible = True
 ws = wb.Worksheets("工作表1")
 
@@ -16,20 +16,21 @@ for line in f:
     str_list = line.split(',')
   
     if str_list[0] == "WRITE":
-        ws.Cells(str_list[1],str_list[2]).Value = str_list[3]
-        s.write("OK")
+        ws.Cells(int(str_list[1]),int(str_list[2])).Value = str_list[3]
+        s.write("OK\n")
         wb.Save()
         
     elif str_list[0] == "Get_Data":
         get_data_list = ws.Range(ws.Cells(1,1), ws.Cells(200,100)).Value
         for i in range (200):
             for j in range(100):
-                s.write(str(get_data_list[i][j]) + ",")
-            print(";")
+                if str(get_data_list[i][j]) == 'None':
+                    s.write(",")
+                else:
+                    s.write(str(get_data_list[i][j]) + ",")
+            s.write(";")
         
     else:
         print("para must be 'write' or 'get_data'")
 
         
-#excel 欄位取值
-        #ws.Range(A1.Value)
